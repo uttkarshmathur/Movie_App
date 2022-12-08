f01892ed344bbcde85c30d5d5e308d03f34de9dd
uttkarshmathur <119284188+uttkarshmathur@users.noreply.github.com>
2022-12-08 18:34:19 +0530

    Movie_App(all files)
    This is a basic layout of a movie web application which do not refreshes every time we search for a movie , the docx file contains screenshot for two of the search results.

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React,{ useState,useEffect} from "react";
+
+import "./App.css";
+import MovieCard from "./MovieCard";
+import searchIcon from "./search.svg";
+
+const API_URL="http://www.omdbapi.com?apikey=a4529e50";
+
+const App=()=>{
+    const[searchTerm, setSearchTerm]=useState("");
+    const[movies, setMovies]=useState([]);
+
+    useEffect(()=>{
+        searchMovies('spiderman');
+      },[]);
+    
+    const searchMovies=async(title)=>{
+      const response=await fetch(`${API_URL}&s=${title}`);
+      const data=await response.json();
+
+     setMovies(data.Search);
+    }
+   
+    return(
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                   value={searchTerm}
+                   onChange={(e)=>
+                   setSearchTerm(e.target.value)}
+                   placeholder="Search for Movies" />
+                   <img src={searchIcon}
+                   alt="Search" onClick={()=>searchMovies(searchTerm)} />
+            </div>
+            {movies?.length >0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ): (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+            )}
+            </div>
+    );
+};
+
+export default App;
